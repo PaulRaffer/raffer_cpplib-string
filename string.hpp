f4d10409acb6f5ff8c5df5ffeb352d4dfbf0d8f9
Paul Raffer <paul.raffer@protonmail.com>
2020-12-18 21:16:55 +0100

    history is stored in repository 'raffer_cpplib-old'

---
+++ string.hpp
@@ -0,0 +1,67 @@
+
+// Copyright (c) 2020 Paul Raffer.
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// https://www.boost.org/LICENSE_1_0.txt)
+
+#if __cplusplus >= 201703L
+
+#ifndef RAFFER_STRING_STRING_HPP
+#define RAFFER_STRING_STRING_HPP
+
+
+#include <string>
+#include <sstream>
+#include <cstring>
+
+#include <locale>
+#include <codecvt>
+
+
+namespace raffer // interface
+{
+
+template <typename Char, int precision = 6, typename T>
+[[nodiscard]] auto to_basic_string(T value) -> std::basic_string<Char>;
+
+template <>
+[[nodiscard]] auto to_basic_string<char>(std::wstring wstr) -> std::string;
+
+//template <>
+//[[nodiscard]] auto to_basic_string<char>(std::wstring wstr);
+
+[[nodiscard]] auto to_string(std::wstring const & wstr) -> std::string;
+[[nodiscard]] auto to_wstring(std::string const & str) -> std::wstring;
+
+[[nodiscard]] auto to_char_array(char const * char_array) -> char const *;
+[[nodiscard]] auto to_char_array(wchar_t const * wchar_array) -> char const *;
+
+
+
+[[nodiscard]] auto length(std::string const & str) -> size_t;
+[[nodiscard]] auto length(std::wstring const & str) -> size_t;
+
+} // namespace raffer
+
+
+
+
+
+namespace raffer // implementation
+{
+
+template <typename Char, int precision, typename T>
+auto to_basic_string(T value) -> std::basic_string<Char>
+{
+	auto oss = std::basic_ostringstream<Char>{};
+	oss.precision(precision);
+	oss << value;
+	return oss.str();
+}
+
+} // namespace raffer
+
+
+#endif // RAFFER_STRING_STRING_HPP
+
+#endif // __cplusplus >= 201703L
